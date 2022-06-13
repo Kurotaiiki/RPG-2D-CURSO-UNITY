b70e055667bf4b40b975218c76c576df752f3783
Andres Felipe Vargas <pipevarg1230@gmail.com>
2022-06-12 22:27:57 -0500

    .

---
+++ Assets/Scripts/InputPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour {
+
+	
+
+	[HideInInspector] public float ejeHorizontal {get ;private set;}
+	[HideInInspector] public float ejeVertical	 {get ;private set;}
+	[HideInInspector] public bool atacar		 {get ;private set;}
+	[HideInInspector] public bool habilidad1	 {get ;private set;}
+	[HideInInspector] public bool habilidad2	 {get ;private set;}
+	[HideInInspector] public bool inventario	 {get ;private set;}
+	[HideInInspector] public bool interactuar	{get; private set;}
+	
+	void start()
+    {
+
+    }
+
+
+	void Update () {
+		atacar = Input.GetButtonDown("Atacar");
+		habilidad1 = Input.GetButtonDown("Habilidad1");
+		habilidad2 = Input.GetButtonDown("Habilidad2");
+		inventario = Input.GetButtonDown("Inventario");
+		interactuar = Input.GetButtonDown("Interactuar");
+
+		//Definir ejes de movimiento
+		ejeHorizontal = Input.GetAxis("Horizontal");
+		ejeVertical = Input.GetAxis("Vertical");
+
+
+
+
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float velocidad = 3;
+
+	private InputPlayer inputJugador;
+	private float horizontal;
+	private float vertical;
+	private Rigidbody2D miRigidbody2D;
+	private Animator animator;
+	private SpriteRenderer miSprite;
+    int correrHashCode;
+
+
+	void Start () {
+		inputJugador = GetComponent<InputPlayer>();
+		miRigidbody2D = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		miSprite = GetComponent<SpriteRenderer>();
+        correrHashCode = Animator.StringToHash("Corriendo");
+		
+	}
+
+
+
+	void Update()
+    {
+        horizontal = inputJugador.ejeHorizontal;
+        vertical = inputJugador.ejeVertical;
+
+        //VoltearSprite();
+
+        if (vertical != 0 || horizontal != 0)
+        {
+            SetearXYAnimator();
+            animator.SetBool(correrHashCode, true);
+        }
+        else
+        {
+            animator.SetBool(correrHashCode, false);
+            Debug.Log("pruebas");
+        }
+
+    }
+
+
+
+
+    private void VoltearSprite()
+    {
+        if (horizontal > 0 && Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+        {
+            miSprite.flipX = true;
+        }
+        else if (horizontal != 0)
+        {
+            miSprite.flipX = false;
+        }
+    }
+
+    private void SetearXYAnimator()
+    {
+        animator.SetFloat("X", horizontal);
+        animator.SetFloat("Y", vertical);
+    }
+
+    void FixedUpdate () {
+
+		#region MOVIMIENTO 
+
+		Vector2 vectorVelocidad = new Vector2 (horizontal, vertical)*velocidad;
+		miRigidbody2D.velocity = vectorVelocidad;
+
+        #endregion MOVIMIENTO
+
+    }
+
+
+}
