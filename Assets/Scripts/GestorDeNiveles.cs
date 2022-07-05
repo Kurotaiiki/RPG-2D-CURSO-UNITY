939e5324084da7b7ab54ed972d225f09b0342497
Andres Felipe Vargas <pipevarg1230@gmail.com>
2022-07-05 00:19:39 -0500

    lo

---
+++ Assets/Scripts/GestorDeNiveles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestorDeNiveles : MonoBehaviour {
+
+	// Use this for initialization
+	public void CargarSiguienteNivel()
+    {
+        int escenaActualIndice = SceneManager.GetActiveScene().buildIndex;
+        int siguienteEscenaIndice = ++escenaActualIndice;
+        SceneManager.LoadScene(siguienteEscenaIndice);
+    }
+}
