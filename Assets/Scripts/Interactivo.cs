42f087a335c6561de6a08dcbaca6e82f544b535d
Andres Felipe Vargas <pipevarg1230@gmail.com>
2022-07-05 00:19:39 -0500

    .

---
+++ Assets/Scripts/Interactivo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactivo : MonoBehaviour {
+
+	private Collider2D miColisionador;
+	public UnityEvent onInteraccion;
+
+
+
+
+
+	void Start () 
+	{
+		miColisionador = GetComponent<Collider2D>();	
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+		onInteraccion.Invoke();
+    }
+	
+	
+}
